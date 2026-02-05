5b69b6631cc89002bb06a32a143d01b6cc19ef80
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2026-02-04 21:35:55 -0300

    Fix => Resolver problema de cors em dev

--- api/main.py
+++ api/main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import router
 
 from config.env_settings import get_settings
 
 app = FastAPI(
     title="Law Agents API",
-    description="API para obter respostas via agentes de IA sobre assuntos legais tributários",
+    description="API para obter respostas via agentes de IA em documentos importados",
     version="0.1.0",
 )
 
-app.include_router(router, prefix="/law-agents", tags=["Law Agents"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
