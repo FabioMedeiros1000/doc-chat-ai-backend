8c9085907d8b21e10437c741316f0b6f588f6482
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2026-02-07 10:32:28 -0300

    Remove => Remover código desnecessário

--- agents/chat_responder.py
+++ agents/chat_responder.py
@@ -1,12 +1,10 @@
 from agno.agent import Agent, RunOutput
 from config.llm_settings import get_llm, LLMModel
-from vectordb.knowledge import knowledge
 from prompts.chat_responder import instructions
 
 agent = Agent(
     model=get_llm(LLMModel.SMALL),
     instructions=instructions,
-    knowledge=knowledge,
     search_knowledge=True,
     add_knowledge_to_context=True
 )
