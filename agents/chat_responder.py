2d5d04038ed1672e30cea43337dd9b8590696547
Fabio <fabio.leandro@fan-edu.com.br>
2026-04-15 09:07:43 -0300

    Refactor => Modificar configurações do agente

--- agents/chat_responder.py
+++ agents/chat_responder.py
@@ -14,7 +14,8 @@ def get_chat_responder_agent(knowledge) -> Agent:
         db=get_db_for_agent(),
         add_memories_to_context=True,
         add_history_to_context=True,
-        num_history_runs=5
+        num_history_runs=5,
+        markdown=False
     )
 
     return agent
